7b8fb67335c965e2dcdeaa885962d3dc880f7d3f
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-11-07 14:06:46 +0800

    ✨ feat: add home page

---
+++ src/views/home/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { } from "vue"
+import { Tabs, TabsList, TabsContent, TabsListItem } from "@components/tabs"
+import { Calendar } from "@components/calendar"
+</script>
+
+<template>
+    <div class="contenxt w-full">
+        <Tabs default-value="account">
+            <TabsList default-value="2">
+                <TabsListItem value="1">1</TabsListItem>
+                <TabsListItem value="2">2</TabsListItem>
+                <TabsListItem value="3">3</TabsListItem>
+            </TabsList>
+            <TabsContent>
+
+            </TabsContent>
+        </Tabs>
+        <Calendar/>
+    </div>
+</template>
