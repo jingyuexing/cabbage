a34d19e744b263964e3c031ed4c99f02a05a1b6b
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-11-07 14:07:03 +0800

    ✨ feat: add user page

---
+++ src/views/user/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+    import { ref } from "vue"
+    import { AAvatar } from "anu-vue"
+    const avatar = ref<typeof AAvatar>();
+</script>
+
+<template>
+    <div class="user">
+        hello
+    </div>
+</template>
