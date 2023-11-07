05e21c19e64b1f90c6805051006701bd721146b8
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-11-07 14:05:33 +0800

    ✨ feat: add calendar components

---
+++ src/components/calendar/calendarContent.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+    
+</script>
+<template>
+    <div class="grid grid-cols-7 gap-1">
+        <slot></slot>
+    </div>
+</template>
---
+++ src/components/calendar/calendarItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import { defineProps } from "vue"
+    interface CalendarItemProps {
+        value:string
+        key:string
+        current?:boolean
+        disable?:boolean
+    }
+    const props = defineProps<CalendarItemProps>()
+
+    const value = props.value
+</script>
+
+<template>
+    <div class="calendar-item relative p-2 flex justify-center text-center rounded-lg box-border items-center text-center" v-bind="props">
+        <slot></slot>
+    </div>
+</template>
